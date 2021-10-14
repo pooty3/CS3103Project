749ca1b44d7bc2aaba56121c9c3dc910fc145480
pooty3 <aloysiuslim.dw@gmail.com>
2021-10-14 22:21:38 +0800

    msg

--- main.cpp
+++ main.cpp
@@ -8,13 +8,32 @@
 #include <sstream>
 #include <string>
 #include <vector>
-
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#define BUFFER_SIZE 500000
 using namespace std;
-
+int connectToServer(const char* hostname, const char* port) {
+    struct addrinfo hints;
+    struct addrinfo *destAddr;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    getaddrinfo(hostname, port, &hints, &destAddr);
+    int socketfd = socket(destAddr -> ai_family, destAddr -> ai_socktype,
+    destAddr -> ai_protocol);
+    connect(socketfd, destAddr->ai_addr, destAddr->ai_addrlen);
+    cout<<"connected to server!\n";
+    freeaddrinfo(destAddr);
+    return socketfd;
+}
+void sendToServer(char* buffer, int socket) {
+    
+}
 void handleConnection(int socket) {
     cout<<"accepted\n";
-    char buffer[100000] = {0};
-    int valred = read(socket, buffer, 100000);
+    char buffer[BUFFER_SIZE] = {0};
+    int valred = read(socket, buffer, BUFFER_SIZE);
     cout<<"message:"<<"\n";
     cout<<buffer<<"\n"; 
     stringstream ss;
@@ -25,9 +44,15 @@ void handleConnection(int socket) {
         tokens.push_back(tok);
     }
     auto pos = tokens[1].find(":");
-    string hostname = tokens[1].substr(0, pos);
-    int port = stoi(tokens[1].substr(pos+1));
+    const char* hostname = (tokens[1].substr(0, pos)).c_str();
+    const char* port = (tokens[1].substr(pos+1)).c_str();
     cout<<hostname<<" "<<port<<"\n";
+    int serversocket = connectToServer(hostname, port);
+
+    size_t msglen = recv(serversocket, buffer, BUFFER_SIZE, 0);
+    cout<<"replying:"
+    cout<<msglen<<":"<<"\n";
+    cout<<buffer;
 }
 int main(int argc, char const *argv[]) {
     struct sockaddr_in address;
